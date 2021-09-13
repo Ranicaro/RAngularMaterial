f742fabba326b61eadbbc12e0a04e007b2eefbac
Ranicaro <nicolascaceresrom@hotmail.com>
2021-09-12 22:49:26 -0500

    cambios tabla y progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TablaComponent } from './page/tabla/tabla.component';
+import { TabloComponent } from './page/tablo/tablo.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent,
+    TabloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page/tablo/tablo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tablo',
+  templateUrl: './tablo.component.html',
+  styleUrls: ['./tablo.component.css']
+})
+export class TabloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
